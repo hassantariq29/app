9f7235241b8d88734c1671a6d002f18debde1b35
root <root@goubuntu.(none)>
2015-12-16 23:07:23 +0500

    initial

---
+++ Http/Controllers/ServerController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\View;
+use Mail;
+class ServerController extends Controller
+{
+ 
+	
+	public function getIndex()
+    {
+    	\SSH::into('production')->run(array(
+	    'cd ~/sample',
+	    'git pull origin master'
+		), function($line){
+	
+				echo $line.PHP_EOL; // outputs server feedback
+			});
+    	//return "Mail Sent !!";
+	
+    }
+    
+   	
+    
+ }
+ 
